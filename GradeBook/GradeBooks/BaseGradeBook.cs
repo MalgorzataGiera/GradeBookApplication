69f656d25832679f995ce9bb4bf118c550d60bb0
unknown <gosgie02@wp.pl>
2023-03-09 21:01:03 +0100

    Update BaseGradeBook.GetGPA

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -119,18 +119,21 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var bonus = 0;
+            if((IsWeighted == true && studentType.ToString() == "Honors") || (IsWeighted == true && studentType.ToString() == "DualEnrolled")) bonus = 1;
             switch (letterGrade)
             {
+                
                 case 'A':
-                    return 4;
+                    return 4 + bonus;
                 case 'B':
-                    return 3;
+                    return 3 + bonus;
                 case 'C':
-                    return 2;
+                    return 2 + bonus;
                 case 'D':
-                    return 1;
+                    return 1 + bonus;
                 case 'F':
-                    return 0;
+                    return 0 + bonus;
             }
             return 0;
         }
